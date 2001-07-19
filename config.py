754d9aedbef54b97b8f36eafc29fc64887bb9bfb
Anthony Baxter <anthonybaxter@users.sourceforge.net>
2001-07-19 05:46:47 +0000

    modified to use localconfig.py (if it exists)
    and to make the various options (e.g. paths) based on ROUNDUP_HOME &c.

--- config.py
+++ config.py
@@ -1,14 +1,34 @@
-# This is the directory that the database is going to be stored in
-DATABASE = '/home/httpd/html/roundup/db'
+ROUNDUP_HOME=MAIL_DOMAIN=MAILHOST=None
 
-# The email address that mail to roundup should go to
-ISSUE_TRACKER_EMAIL = 'issue_tracker@bizarsoftware.com.au'
+try:
+    from localconfig import *
+except ImportError:
+    localconfig = None
 
-# The email address that roundup will complain to if it runs into trouble
-ADMIN_EMAIL = "roundup-admin@bizarsoftware.com.au"
+import os
+
+# This is the root directory for roundup
+if not ROUNDUP_HOME:
+    ROUNDUP_HOME='/home/httpd/html/roundup'
 
 # The SMTP mail host that roundup will use to send mail
-MAILHOST = 'dirk'
+if not MAILHOST:
+    MAILHOST = 'localhost'
+
+# The domain name used for email addresses.
+if not MAIL_DOMAIN:
+    MAIL_DOMAIN = 'bizarsoftware.com.au'
+
+# This is the directory that the database is going to be stored in
+DATABASE = os.path.join(ROUNDUP_HOME, 'db')
+
+# The email address that mail to roundup should go to
+ISSUE_TRACKER_EMAIL = 'issue_tracker@%s'%MAIL_DOMAIN
+
+# The email address that roundup will complain to if it runs into trouble
+ADMIN_EMAIL = 'roundup-admin@%s'%MAIL_DOMAIN
 
 # Somewhere for roundup to log stuff internally sent to stdout or stderr
-LOG = '/home/httpd/html/roundup/roundup.log'
+LOG = os.path.join(ROUNDUP_HOME, 'roundup.log')
+
+del os
