c82ac0f3f4c8b96eaed7f58aeff307478f715c52
Richard Jones <richard@users.sourceforge.net>
2001-07-23 23:16:01 +0000

    Split off the interfaces (CGI, mailgw) into a separate file from the DB stuff.

--- roundup/templates/extended/__init__.py
+++ roundup/templates/extended/__init__.py
@@ -1,8 +1,12 @@
-# $Id: __init__.py,v 1.2 2001-07-23 04:33:21 anthonybaxter Exp $
+# $Id: __init__.py,v 1.3 2001-07-23 23:16:01 richard Exp $
 
 from instance_config import *
 from dbinit import *
+from interfaces import *
 
 # 
 # $Log: not supported by cvs2svn $
+# Revision 1.2  2001/07/23 04:33:21  anthonybaxter
+# split __init__.py into 2. dbinit and instance_config.
+#
 #
---
+++ roundup/templates/extended/interfaces.py
@@ -0,0 +1,25 @@
+# $Id: interfaces.py,v 1.1 2001-07-23 23:16:01 richard Exp $
+
+import instance_config
+from roundup import cgi_client, mailgw 
+
+class Client(cgi_client.Client): 
+    ''' derives basic mail gateway implementation from the standard module, 
+        with any specific extensions 
+    ''' 
+    TEMPLATES = instance_config.TEMPLATES
+    pass 
+ 
+class MailGW(mailgw.MailGW): 
+    ''' derives basic mail gateway implementation from the standard module, 
+        with any specific extensions 
+    ''' 
+    ISSUE_TRACKER_EMAIL = instance_config.ISSUE_TRACKER_EMAIL
+    ADMIN_EMAIL = instance_config.ADMIN_EMAIL
+    MAILHOST = instance_config.MAILHOST
+
+#
+# $Log: not supported by cvs2svn $
+#
+
+
