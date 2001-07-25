e833bd40ff65c6802d52cda6510b1f5d227f80e7
Richard Jones <richard@users.sourceforge.net>
2001-07-25 04:10:15 +0000

    added date test suite

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,10 @@
 import unittest
 
-import test_schema, test_db
+import test_dates, test_schema, test_db
 
 def go():
     suite = unittest.TestSuite((
+        test_dates.suite(),
         test_schema.suite(),
         test_db.suite(),
     ))
