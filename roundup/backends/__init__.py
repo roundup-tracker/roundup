7794f1e217bfcfa6619662c525d826b214c53ba9
Richard Jones <richard@users.sourceforge.net>
2001-07-30 03:52:16 +0000

    Checks for ability to import the specific back-end module.

--- roundup/backends/__init__.py
+++ roundup/backends/__init__.py
@@ -1,4 +1,26 @@
-import back_bsddb; bsddb = back_bsddb
-import back_anydbm; anydbm = back_anydbm
+__all__ = []
 
-__all__ = ['bsddb', 'anydbm']
+try:
+    import back_anydbm
+    anydbm = back_anydbm
+    del back_anydbm
+    __all__.append('anydbm')
+except:
+    pass
+
+try:
+    import back_bsddb
+    bsddb = back_bsddb
+    del back_bsddb
+    __all__.append('bsddb')
+except:
+    pass
+
+try:
+    import back_bsddb3
+    bsddb3 = back_bsddb3
+    del back_bsddb3
+    __all__.append('bsddb3')
+except:
+    pass
+
