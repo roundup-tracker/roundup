5141cd1e20521b0dbd1a41c506ebd1129b12612c
Richard Jones <richard@users.sourceforge.net>
2001-07-30 01:28:17 +0000

    Support for determining the installed tempaltes

--- roundup/templates/__init__.py
+++ roundup/templates/__init__.py
@@ -1,2 +1,12 @@
+import os
 
+def listTemplates():
+    t_dir = os.path.split(__file__)[0]
+    l = []
+    for entry in os.listdir(t_dir):
+        # this isn't strictly necessary - the CVS dir won't be distributed
+        if entry == 'CVS': continue
+        if os.path.isdir(os.path.join(t_dir, entry)):
+            l.append(entry)
+    return l
 
