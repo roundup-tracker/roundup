7d2e3d479065eeabfe510ce3bb93a051e2410299
Richard Jones <richard@users.sourceforge.net>
2001-07-30 01:25:57 +0000

    Changes to reflect cgi_client now implementing classic by default...
    ...and not "extended".

--- roundup/templates/classic/interfaces.py
+++ roundup/templates/classic/interfaces.py
@@ -1,4 +1,4 @@
-# $Id: interfaces.py,v 1.3 2001-07-29 07:01:39 richard Exp $
+# $Id: interfaces.py,v 1.4 2001-07-30 01:25:57 richard Exp $
 
 import urlparse, os
 
@@ -11,36 +11,6 @@ class Client(cgi_client.Client):
     ''' 
     TEMPLATES = instance_config.TEMPLATES
 
-    default_index_sort = ['-activity']
-    default_index_group = ['priority']
-    default_index_filter = []
-    default_index_columns = ['id','activity','title','status','assignedto']
-    default_index_filterspec = {'status': ['1', '2', '3', '4', '5', '6', '7']}
-
-    def pagehead(self, title, message=None):
-        url = self.env['SCRIPT_NAME'] + '/' #self.env.get('PATH_INFO', '/')
-        machine = self.env['SERVER_NAME']
-        port = self.env['SERVER_PORT']
-        if port != '80': machine = machine + ':' + port
-        base = urlparse.urlunparse(('http', machine, url, None, None, None))
-        if message is not None:
-            message = '<div class="system-msg">%s</div>'%message
-        else:
-            message = ''
-        style = open(os.path.join(self.TEMPLATES, 'style.css')).read()
-        userid = self.db.user.lookup(self.user)
-        self.write('''<html><head>
-<title>%s</title>
-<style type="text/css">%s</style>
-</head>
-<body bgcolor=#ffffff>
-%s
-<table width=100%% border=0 cellspacing=0 cellpadding=2>
-<tr class="location-bar"><td><big><strong>%s</strong></big>
-(login: <a href="user%s">%s</a>)</td></tr>
-</table>
-'''%(title, style, message, title, userid, self.user))
- 
 class MailGW(mailgw.MailGW): 
     ''' derives basic mail gateway implementation from the standard module, 
         with any specific extensions 
@@ -51,6 +21,9 @@ class MailGW(mailgw.MailGW):
 
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.3  2001/07/29 07:01:39  richard
+# Added vim command to all source so that we don't get no steenkin' tabs :)
+#
 # Revision 1.2  2001/07/29 04:07:37  richard
 # Fixed the classic template so it's more like the "advertised" Roundup
 # template.
