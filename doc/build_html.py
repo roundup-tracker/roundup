371ff690d67fedbc0100304ff08c5ec98375d84b
Richard Jones <richard@users.sourceforge.net>
2002-05-06 23:38:07 +0000

    update for (dps+restructuretext) -> docutils

--- doc/build_html.py
+++ doc/build_html.py
@@ -3,8 +3,8 @@
 """
 :Author: David Goodger
 :Contact: goodger@users.sourceforge.net
-:Revision: $Revision: 1.1 $
-:Date: $Date: 2002-03-08 23:41:46 $
+:Revision: $Revision: 1.2 $
+:Date: $Date: 2002-05-06 23:38:07 $
 :Copyright: This module has been placed in the public domain.
 
 A minimal front-end to the Docutils Publisher.
@@ -13,8 +13,8 @@ This module takes advantage of the default values defined in `publish()`.
 """
 
 import sys, os.path
-from dps.core import publish
-from dps import utils
+from docutils.core import publish
+from docutils import utils
 
 if len(sys.argv) < 2:
     print >>sys.stderr, 'I need at least one filename'
@@ -26,6 +26,6 @@ for file in sys.argv[1:]:
     name, ext = os.path.splitext(file)
     dest = '%s.html'%name
     print >>sys.stderr, '%s -> %s'%(file, dest)
-    publish(writername='html', source=file, destination=dest,
+    publish(writer_name='html', source=file, destination=dest,
         reporter=reporter)
 
