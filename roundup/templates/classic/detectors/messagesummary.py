ad8887efcb399a592ca9653ce9aa832290f03dd8
Richard Jones <richard@users.sourceforge.net>
2003-02-13 21:14:27 +0000

    oops, missing from yesterday's checkin

---
+++ roundup/templates/classic/detectors/messagesummary.py
@@ -0,0 +1,19 @@
+#$Id: messagesummary.py,v 1.1 2003-02-13 21:14:27 richard Exp $
+
+from roundup.mailgw import parseContent
+
+def summarygenerator(db, cl, nodeid, newvalues):
+    ''' If the message doesn't have a summary, make one for it.
+    '''
+    if newvalues.has_key('summary') or not newvalues.has_key('content'):
+        return
+
+    summary, content = parseContent(newvalues['content'], 1, 1)
+    newvalues['summary'] = summary
+
+
+def init(db):
+    # fire before changes are made
+    db.msg.audit('create', summarygenerator)
+
+# vim: set filetype=python ts=4 sw=4 et si
