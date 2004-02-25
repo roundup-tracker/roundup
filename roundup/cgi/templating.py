67953357b593218278847e05054f30bcba7095ac
Richard Jones <richard@users.sourceforge.net>
2004-02-25 09:40:46 +0000

    forward-port of fix from maint-0-6

--- roundup/cgi/templating.py
+++ roundup/cgi/templating.py
@@ -276,7 +276,10 @@ class HTMLDatabase:
             m.append(HTMLClass(self._client, item))
         return r
 
-def lookupIds(db, prop, ids, num_re=re.compile('-?\d+')):
+def lookupIds(db, prop, ids, fail_ok=False, num_re=re.compile('-?\d+')):
+    ''' "fail_ok" should be specified if we wish to pass through bad values
+        (most likely form values that we wish to represent back to the user)
+    '''
     cl = db.getclass(prop.classname)
     l = []
     for entry in ids:
@@ -285,9 +288,10 @@ def lookupIds(db, prop, ids, num_re=re.compile('-?\d+')):
         else:
             try:
                 l.append(cl.lookup(entry))
-            except KeyError:
-                # ignore invalid keys
-                pass
+            except (TypeError, KeyError):
+                if fail_ok:
+                    # pass through the bad value
+                    l.append(entry)
     return l
 
 class HTMLPermissions:
@@ -382,11 +386,12 @@ class HTMLClass(HTMLInputMixin, HTMLPermissions):
             if form.has_key(item):
                 if isinstance(prop, hyperdb.Multilink):
                     value = lookupIds(self._db, prop,
-                        handleListCGIValue(form[item]))
+                        handleListCGIValue(form[item]), fail_ok=True)
                 elif isinstance(prop, hyperdb.Link):
                     value = form[item].value.strip()
                     if value:
-                        value = lookupIds(self._db, prop, [value])[0]
+                        value = lookupIds(self._db, prop, [value],
+                            fail_ok=True)[0]
                     else:
                         value = None
                 else:
