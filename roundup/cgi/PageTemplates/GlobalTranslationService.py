69a57fe7f0f496e49ec9e12b05dd6b409fe3a3fb
Alexander Smishlajev <a1s@users.sourceforge.net>
2004-05-29 00:08:07 +0000

    roll back to revision 1.1: since we aren't using DummyTranslationService...
    ...there is no need to patch it.
    
    roundup translation service moved to roundup.cgi.templating

--- roundup/cgi/PageTemplates/GlobalTranslationService.py
+++ roundup/cgi/PageTemplates/GlobalTranslationService.py
@@ -14,15 +14,12 @@
 # Modifications for Roundup:
 # 1. implemented ustr as str
 # 2. make imports use roundup.cgi
-# 3. added StaticTranslationService
 """Global Translation Service for providing I18n to Page Templates.
 
-$Id: GlobalTranslationService.py,v 1.3 2004-05-23 13:05:43 a1s Exp $
+$Id: GlobalTranslationService.py,v 1.4 2004-05-29 00:08:07 a1s Exp $
 """
 
 import re
-
-from roundup import i18n
 
 from roundup.cgi.TAL.TALDefs import NAME_RE
 
@@ -32,38 +29,13 @@ class DummyTranslationService:
     """Translation service that doesn't know anything about translation."""
     def translate(self, domain, msgid, mapping=None,
                   context=None, target_language=None, default=None):
-        return str(msgid).upper()
         def repl(m, mapping=mapping):
             return ustr(mapping[m.group(m.lastindex)])
         cre = re.compile(r'\$(?:(%s)|\{(%s)\})' % (NAME_RE, NAME_RE))
         return cre.sub(repl, default or msgid)
     # XXX Not all of Zope.I18n.ITranslationService is implemented.
 
-class StaticTranslationService:
-
-    """Translation service for application default language
-
-    This service uses "static" translation, with single domain
-    and target language, initialized from OS environment when
-    roundup.i18n is loaded.
-
-    'domain' and 'target_language' parameters to 'translate()'
-    are ignored.
-
-    Returned strings are always utf8-encoded.
-
-    """
-
-    OUTPUT_ENCODING = "utf-8"
-
-    def translate(self, domain, msgid, mapping=None,
-        context=None, target_language=None, default=None
-    ):
-        _msg = i18n.ugettext(msgid).encode(self.OUTPUT_ENCODING)
-        #print ("TRANSLATE", msgid, _msg, mapping, context)
-        return _msg
-
-translationService = StaticTranslationService()
+translationService = DummyTranslationService()
 
 def setGlobalTranslationService(service):
     """Sets the global translation service, and returns the previous one."""
