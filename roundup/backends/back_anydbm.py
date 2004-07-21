989e78d6f435bdc7879a4b33c98bf1c91ce29261
Richard Jones <richard@users.sourceforge.net>
2004-07-21 00:51:49 +0000

    merge from HEAD

--- roundup/backends/back_anydbm.py
+++ roundup/backends/back_anydbm.py
@@ -15,7 +15,7 @@
 # BASIS, AND THERE IS NO OBLIGATION WHATSOEVER TO PROVIDE MAINTENANCE,
 # SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 # 
-#$Id: back_anydbm.py,v 1.146.2.15 2004-07-20 23:27:02 richard Exp $
+#$Id: back_anydbm.py,v 1.146.2.16 2004-07-21 00:51:49 richard Exp $
 '''This module defines a backend that saves the hyperdatabase in a
 database chosen by anydbm. It is guaranteed to always be available in python
 versions >2.1.1 (the dumbdbm fallback in 2.1.1 and earlier has several
@@ -1429,6 +1429,8 @@ class Class(hyperdb.Class):
             for nodeid in self.getnodeids(cldb):
                 node = self.db.getnode(self.classname, nodeid, cldb)
                 if node.has_key(self.db.RETIRED_FLAG):
+                    continue
+                if not node.has_key(self.key):
                     continue
                 if node[self.key] == keyvalue:
                     return nodeid
