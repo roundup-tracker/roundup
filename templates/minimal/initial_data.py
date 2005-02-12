35220a98484e42c3341be608d2e8e4b4ab3d5416
Richard Jones <richard@users.sourceforge.net>
2005-02-12 00:47:33 +0000

    docs editing from Jean Jordaan

--- templates/minimal/initial_data.py
+++ templates/minimal/initial_data.py
@@ -8,7 +8,7 @@ user.create(username="admin", password=adminpw,
     address=admin_email, roles='Admin')
 user.create(username="anonymous", roles='Anonymous')
 
-# add any additional database create steps here - but only if you
+# add any additional database creation steps here - but only if you
 # haven't initialised the database with the admin "initialise" command
 
 # vim: set et sts=4 sw=4 :
