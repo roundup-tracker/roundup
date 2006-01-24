0ae8f6e50f3eb25379f9062ecc2ebe5741baa219
Alexander Smishlajev <a1s@users.sourceforge.net>
2006-01-24 08:16:59 +0000

    added class PrioList (patch from rfe [SF#413165])

--- roundup/support.py
+++ roundup/support.py
@@ -24,4 +24,38 @@ def ensureParentsExist(dest):
     if not os.path.exists(os.path.dirname(dest)):
         os.makedirs(os.path.dirname(dest))
 
+class PrioList:
+    '''Manages a sorted list.
+
+    Currently only implements method 'append' and iteration from a
+    full list interface.
+    Implementation: We manage a "sorted" status and sort on demand.
+    Appending to the list will require re-sorting before use.
+    >>> p = PrioList ()
+    >>> for i in 5,7,1,-1 :
+    ...  p.append (i)
+    ...
+    >>> for k in p :
+    ...  print k
+    ...
+    -1
+    1
+    5
+    7
+
+    '''
+    def __init__(self):
+        self.list   = []
+        self.sorted = True
+
+    def append(self, item):
+        self.list.append (item)
+        self.sorted = False
+
+    def __iter__(self):
+        if not self.sorted :
+            self.list.sort ()
+            self.sorted = True
+        return iter (self.list)
+
 # vim: set et sts=4 sw=4 :
