38ed5479dc94b43dfd8d46ba79ae54310902e4d3
Richard Jones <richard@users.sourceforge.net>
2006-03-03 02:29:10 +0000

    pre-release stuff

--- roundup/__init__.py
+++ roundup/__init__.py
@@ -15,7 +15,7 @@
 # BASIS, AND THERE IS NO OBLIGATION WHATSOEVER TO PROVIDE MAINTENANCE,
 # SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 # 
-# $Id: __init__.py,v 1.36 2006-02-08 04:46:41 richard Exp $
+# $Id: __init__.py,v 1.37 2006-03-03 02:29:10 richard Exp $
 
 '''Roundup - issue tracking for knowledge workers.
 
@@ -68,6 +68,6 @@ much prettier cake :)
 '''
 __docformat__ = 'restructuredtext'
 
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 
 # vim: set filetype=python ts=4 sw=4 et si
