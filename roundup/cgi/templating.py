79c645cc4d71466aa3c34c290df6d6b46683e935
Richard Jones <richard@users.sourceforge.net>
2006-08-11 01:24:47 +0000

    *cough*

--- roundup/cgi/templating.py
+++ roundup/cgi/templating.py
@@ -2213,8 +2213,7 @@ class HTMLRequest(HTMLInputMixin):
             # dispname should be encoded to the web interface's charset,
             # but the web interface rendering will be expecting a unicode
             # string
-            charset = self.client.charset
-            self.dispname = value.decode('utf8')
+            self.dispname = value.decode(self.client.charset)
 
     def updateFromURL(self, url):
         ''' Parse the URL for query args, and update my attributes using the
