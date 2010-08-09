7277beabf1272c694b08eb62ef9a220c479cd0eb
Richard Jones <richard@users.sourceforge.net>
2010-08-09 04:52:16 +0000

    remove unnecessary brackets

--- roundup/backends/rdbms_common.py
+++ roundup/backends/rdbms_common.py
@@ -2729,7 +2729,7 @@ class FileClass(hyperdb.FileClass, Class):
         if propname == 'content':
             try:
                 return self.db.getfile(self.classname, nodeid, None)
-            except IOError, (strerror):
+            except IOError, strerror:
                 # BUG: by catching this we donot see an error in the log.
                 return 'ERROR reading file: %s%s\n%s\n%s'%(
                         self.classname, nodeid, poss_msg, strerror)
