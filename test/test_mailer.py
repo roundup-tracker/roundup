2ce6917798a63c16728c919413419612428b025c
Ralf Schlatterbeck <schlatterbeck@users.sourceforge.net>
2011-07-15 10:03:37 +0000

    Fix mailer encoding test:
    - better name to be able to match it when running single test on the
      command line
    - use iso8859-1 instead of latin1, python2.7 will convert latin1 to
      iso8859-1 which made the test fail

--- test/test_mailer.py
+++ test/test_mailer.py
@@ -4,13 +4,13 @@ import unittest
 from roundup import mailer
 
 class EncodingTestCase(unittest.TestCase):
-    def test(self):
+    def testEncoding(self):
         a = lambda n, a, c, o: self.assertEquals(mailer.nice_sender_header(n,
             a, c), o)
-        a('ascii', 'ascii@test.com', 'latin1', 'ascii <ascii@test.com>')
-        a(u'café', 'ascii@test.com', 'latin1',
-            '=?latin1?q?caf=E9?= <ascii@test.com>')
-        a('as"ii', 'ascii@test.com', 'latin1', '"as\\"ii" <ascii@test.com>')
+        a('ascii', 'ascii@test.com', 'iso8859-1', 'ascii <ascii@test.com>')
+        a(u'café', 'ascii@test.com', 'iso8859-1',
+            '=?iso8859-1?q?caf=E9?= <ascii@test.com>')
+        a('as"ii', 'ascii@test.com', 'iso8859-1', '"as\\"ii" <ascii@test.com>')
 
 def test_suite():
     suite = unittest.TestSuite()
