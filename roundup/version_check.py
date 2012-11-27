bd875719b5909fc0c34cb540fe18adde6ed96e63
anatoly techtonik <techtonik@gmail.com>
2012-11-28 00:12:23 +0300

    version_check.py: Bump required Python version from 2.1.1+ to 2.5+

--- roundup/version_check.py
+++ roundup/version_check.py
@@ -1,29 +1,10 @@
 #!/usr/bin/env python
-#
-# Copyright (c) 2001 Bizar Software Pty Ltd (http://www.bizarsoftware.com.au/)
-# This module is free software, and you may redistribute it and/or modify
-# under the same terms as Python, so long as this copyright message and
-# disclaimer are retained in their original form.
-#
-# IN NO EVENT SHALL BIZAR SOFTWARE PTY LTD BE LIABLE TO ANY PARTY FOR
-# DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING
-# OUT OF THE USE OF THIS CODE, EVEN IF THE AUTHOR HAS BEEN ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# BIZAR SOFTWARE PTY LTD SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING,
-# BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE.  THE CODE PROVIDED HEREUNDER IS ON AN "AS IS"
-# BASIS, AND THERE IS NO OBLIGATION WHATSOEVER TO PROVIDE MAINTENANCE,
-# SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 
-"""Enforces the minimum Python version that Roundup requires.
-"""
-__docformat__ = 'restructuredtext'
+# Roundup requires Python 2.5+ as mentioned in doc\installation.txt
+VERSION_NEEDED = (2,5)
 
 import sys
-if not hasattr(sys, 'version_info') or sys.version_info[:3] < (2,1,1):
+if sys.version_info < VERSION_NEEDED:
     print "Content-Type: text/plain\n"
-    print "Roundup requires Python 2.1.1 or newer."
+    print "Roundup requires Python %s.%s or newer." % VERSION_NEEDED
     sys.exit(0)
-
-# vim: set filetype=python ts=4 sw=4 et si
