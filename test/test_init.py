3a461c20553aad35a3044069dd71716776c316d8
anatoly techtonik <techtonik@gmail.com>
2013-08-24 22:12:19 +0300

    init.loadTemplateInfo: Add test

---
+++ test/test_init.py
@@ -0,0 +1,36 @@
+#-*- encoding: utf8 -*-
+
+import unittest, os, pprint, difflib, textwrap
+
+from roundup.init import loadTemplateInfo
+
+
+class TemplateInfoTestCase(unittest.TestCase):
+    def testLoadTemplateInfo(self):
+        path = os.path.join(os.path.dirname(__file__),
+                            '../share/roundup/templates/classic')
+        self.maxDiff = None
+        self.assertEqual(
+            loadTemplateInfo(path),
+            {
+              'description': textwrap.dedent('''\
+                   This is a generic issue tracker that may be used to track bugs,
+                    feature requests, project issues or any number of other types
+                    of issues. Most users of Roundup will find that this template
+                    suits them, with perhaps a few customisations.'''),
+              'intended-for': 'All first-time Roundup users',
+              'name': 'classic',
+              'path': path
+            }
+        )
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TemplateInfoTestCase))
+    return suite
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    unittest.main(testRunner=runner)
+
+# vim: set et sts=4 sw=4 :
