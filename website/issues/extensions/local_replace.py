2f2adde0425e90202acf447be5ad16eae6aed13a
Bernhard Reiter <bernhard@intevation.de>
2014-03-13 09:49:14 +0100

    Website, Issues: local_replace.py general improvements.  * simplified the regular expressions, three into one.  * only allowing one space after r|rev|revision  * better test order to reflect the order of regexs  * tests: indicate in the comment what should fail

--- website/issues/extensions/local_replace.py
+++ website/issues/extensions/local_replace.py
@@ -4,11 +4,7 @@ substitutions = [ (re.compile('debian:\#(?P<id>\d+)'),
                    '<a href="http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=\g<id>">debian#\g<id></a>' ),
                   (re.compile('\#(?P<ws>\s*)(?P<id>\d+)'),
                    "<a href='issue\g<id>'>#\g<ws>\g<id></a>" ),
-                  (re.compile('(?P<prews>\s+)revision(?P<ws>\s*)(?P<revision>\d+)'),
-                   "\g<prews><a href='http://svn.roundup-tracker.org/viewvc/roundup?view=rev&rev=\g<revision>'>revision\g<ws>\g<revision></a>"),
-                  (re.compile('(?P<prews>\s+)rev(?P<ws>\s*)(?P<revision>\d+)'),
-                   "\g<prews><a href='http://svn.roundup-tracker.org/viewvc/roundup?view=rev&rev=\g<revision>'>rev\g<ws>\g<revision></a>"),
-                  (re.compile('(?P<prews>\s+)(?P<revstr>r|r\s+)(?P<revision>\d+)'),
+                  (re.compile('(?P<prews>\s+)(?P<revstr>(revision|rev|r)\s?)(?P<revision>\d+)'),
                    "\g<prews><a href='http://svn.roundup-tracker.org/viewvc/roundup?view=rev&rev=\g<revision>'>\g<revstr>\g<revision></a>"),
                   ]
 
@@ -26,8 +22,11 @@ def init(instance):
 
 if "__main__" == __name__:
     print " debian:#222", local_replace(" debian:#222")
-    print " revision 222", local_replace(" revision 222")
-    print " wordthatendswithr 222", local_replace(" wordthatendswithr 222")
-    print " r222", local_replace(" r222")
-    print " r 222", local_replace(" r 222")
     print " #555", local_replace(" #555")
+    print " revision 222", local_replace(" revision 222")
+    print " r 222", local_replace(" r 222")
+    print " wordthatendswithr 222", local_replace(" wordthatendswithr 222") # should fail
+    print " too many spaces r  222", local_replace(" too many spaces r  222") # should fail
+    print " r7140eb", local_replace(" r7140eb")
+    print " rev7140eb", local_replace(" rev7140eb")
+    print " rev7140eb", local_replace(" rev7140eb")
