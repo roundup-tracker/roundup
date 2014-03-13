3627e6ff9de2c063caec82cf7930f5690b180376
Bernhard Reiter <bernhard@intevation.de>
2014-03-13 10:38:44 +0100

    Website, Issues: local_replace.py fixed hg regularexpressions.

--- website/issues/extensions/local_replace.py
+++ website/issues/extensions/local_replace.py
@@ -4,7 +4,7 @@ substitutions = [ (re.compile('debian:\#(?P<id>\d+)'),
                    '<a href="http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=\g<id>">debian#\g<id></a>' ),
                   (re.compile('\#(?P<ws>\s*)(?P<id>\d+)'),
                    "<a href='issue\g<id>'>#\g<ws>\g<id></a>" ),
-                  (re.compile('(?P<prews>\s+)(?P<revstr>(revision|rev|r)\s?)(?P<revision>[\da-fA-F]+)'),
+                  (re.compile('(?P<prews>^|\s+)(?P<revstr>(revision|rev|r)\s?)(?P<revision>[\da-fA-F]+)(?P<postws>\s+|$)'),
                    "\g<prews><a href='http://sourceforge.net/p/roundup/code/ci/\g<revision>'>\g<revstr>\g<revision></a>"),
                   ]
 
@@ -26,7 +26,8 @@ if "__main__" == __name__:
     print " revision 222", local_replace(" revision 222")
     print " r 222", local_replace(" r 222")
     print " wordthatendswithr 222", local_replace(" wordthatendswithr 222") # should fail
+    print " references", local_replace(" references") # should fail
     print " too many spaces r  222", local_replace(" too many spaces r  222") # should fail
     print " r7140eb", local_replace(" r7140eb")
-    print " rev7140eb", local_replace(" rev7140eb")
-    print " rev7140eb", local_replace(" rev7140eb")
+    print " rev7140eb ", local_replace(" rev7140eb")
+    print "rev7140eb", local_replace("rev7140eb")
