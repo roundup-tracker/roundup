222e7befd2d2aa25e050f3292fa05d8e213ca357
Bernhard Reiter <bernhard@intevation.de>
2014-03-13 11:09:08 +0100

    local_replace.py: "hg cp"ied the new issues.roundup-tracker.org over to the responsive and devel templates as an improved example.

--- share/roundup/templates/responsive/extensions/local_replace.py
+++ share/roundup/templates/responsive/extensions/local_replace.py
@@ -4,12 +4,8 @@ substitutions = [ (re.compile('debian:\#(?P<id>\d+)'),
                    '<a href="http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=\g<id>">debian#\g<id></a>' ),
                   (re.compile('\#(?P<ws>\s*)(?P<id>\d+)'),
                    "<a href='issue\g<id>'>#\g<ws>\g<id></a>" ),
-                  (re.compile('(?P<prews>\s+)revision(?P<ws>\s*)(?P<revision>\d+)'),
-                   "\g<prews><a href='http://svn.roundup-tracker.org/view?rev=\g<revision>&view=rev'>revision\g<ws>\g<revision></a>"),
-                  (re.compile('(?P<prews>\s+)rev(?P<ws>\s*)(?P<revision>\d+)'),
-                   "\g<prews><a href='http://svn.roundup-tracker.org/view?rev=\g<revision>&view=rev'>rev\g<ws>\g<revision></a>"),
-                  (re.compile('(?P<prews>\s+)(?P<revstr>r|r\s+)(?P<revision>\d+)'),
-                   "\g<prews><a href='http://svn.roundup-tracker.org/view?rev=\g<revision>&view=rev'>\g<revstr>\g<revision></a>"),
+                  (re.compile('(?P<prews>^|\s+)(?P<revstr>(revision|rev|r)\s?)(?P<revision>[\da-fA-F]+)(?P<post>\W+|$)'),
+                   "\g<prews><a href='http://sourceforge.net/p/roundup/code/ci/\g<revision>'>\g<revstr>\g<revision></a>\g<post>"),
                   ]
 
 def local_replace(message):
@@ -18,18 +14,21 @@ def local_replace(message):
         message = cre.sub(replacement, message)
 
     return message
-        
-    
-    
+
+
 def init(instance):
     instance.registerUtil('localReplace', local_replace)
-    
+
 
 if "__main__" == __name__:
     print " debian:#222", local_replace(" debian:#222")
-    print " revision 222", local_replace(" revision 222")
-    print " wordthatendswithr 222", local_replace(" wordthatendswithr 222")
-    print " r222", local_replace(" r222")
-    print " r 222", local_replace(" r 222")
     print " #555", local_replace(" #555")
-    
+    print " revision 222", local_replace(" revision 222")
+    print " r 222", local_replace(" r 222")
+    print " wordthatendswithr 222", local_replace(" wordthatendswithr 222") # should fail
+    print " references", local_replace(" references") # should fail
+    print " too many spaces r  222", local_replace(" too many spaces r  222") # should fail
+    print " r7140eb", local_replace(" r7140eb")
+    print " rev7140eb ", local_replace(" rev7140eb ")
+    print "rev7140eb", local_replace("rev7140eb")
+    print "rev7140eb,", local_replace("rev7140eb,")
