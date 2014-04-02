7f332975de577102f75575461db19c4cc32d5c81
Bernhard Reiter <bernhard@intevation.de>
2014-04-02 12:37:01 +0200

    website: local_replace.py: fixed one corner case for hg revisions. Better test. A minus is not starting a revision anymore

--- website/issues/extensions/local_replace.py
+++ website/issues/extensions/local_replace.py
@@ -6,7 +6,7 @@ substitutions = [ (re.compile('debian:\#(?P<id>\d+)'),
                    "<a href='issue\g<id>'>#\g<ws>\g<id></a>" ),
                   (re.compile('(?P<prews>^|\s+)issue(?P<ws>\s*)(?P<id>\d+)'),
                    "\g<prews><a href='issue\g<id>'>issue\g<ws>\g<id></a>" ),
-                  (re.compile('(?P<prews>^|\s+)(?P<revstr>(revision|rev|r)\s?)(?P<revision>[\da-fA-F]+)(?P<post>\W+|$)'),
+                  (re.compile('(?P<prews>^|\s+)(?P<revstr>(revision|rev|r)\s?)(?P<revision>[1-9a-fA-F][0-9a-fA-F]*)(?P<post>\W+|$)'),
                    "\g<prews><a href='http://sourceforge.net/p/roundup/code/ci/\g<revision>'>\g<revstr>\g<revision></a>\g<post>"),
                   ]
 
@@ -21,17 +21,25 @@ def local_replace(message):
 def init(instance):
     instance.registerUtil('localReplace', local_replace)
 
+def quicktest(msgstr, should_replace = True):
+    if not should_replace:
+        print "(no)",
+    print "'%s' -> '%s'" % (msgstr, local_replace(msgstr))
 
 if "__main__" == __name__:
-    print " debian:#222", local_replace(" debian:#222")
-    print " #555", local_replace(" #555")
-    print "issue333", local_replace("issue333")
-    print " revision 222", local_replace(" revision 222")
-    print " r 222", local_replace(" r 222")
-    print " wordthatendswithr 222", local_replace(" wordthatendswithr 222") # should fail
-    print " references", local_replace(" references") # should fail
-    print " too many spaces r  222", local_replace(" too many spaces r  222") # should fail
-    print " r7140eb", local_replace(" r7140eb")
-    print " rev7140eb ", local_replace(" rev7140eb ")
-    print "rev7140eb", local_replace("rev7140eb")
-    print "rev7140eb,", local_replace("rev7140eb,")
+    print "Replacement examples. '(no)' should result in no replacement:"
+    quicktest(" debian:#222")
+    quicktest(" #555")
+    quicktest("issue333")
+    quicktest(" revision 222")
+    quicktest(" r 222")
+    quicktest(" wordthatendswithr 222", False)
+    quicktest(" references", False)
+    quicktest(" too many spaces r  222", False)
+    quicktest("re-evaluate", False)
+    quicktest("rex140eb", False)
+    quicktest("re140eb")
+    quicktest(" r7140eb")
+    quicktest(" rev7140eb ")
+    quicktest("rev7140eb")
+    quicktest("rev7140eb,")
