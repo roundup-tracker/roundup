9c9f12e2ef0f8067a0d2ea3bec1a91ed80bcc6c1
Bernhard Reiter <bernhard@intevation.de>
2014-04-26 23:13:25 +0200

    Improved website/issues/extensions/local_replace.py and hg copied it over to the devel and responsive templates. * Added a substitution for the default hg output format. * Used a base url variable for the two hg substitutions.

--- share/roundup/templates/devel/extensions/local_replace.py
+++ share/roundup/templates/devel/extensions/local_replace.py
@@ -1,11 +1,20 @@
 import re
 
-substitutions = [ (re.compile('debian:\#(?P<id>\d+)'),
-                   '<a href="http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=\g<id>">debian#\g<id></a>' ),
-                  (re.compile('\#(?P<ws>\s*)(?P<id>\d+)'),
-                   "<a href='issue\g<id>'>#\g<ws>\g<id></a>" ),
-                  (re.compile('(?P<prews>^|\s+)(?P<revstr>(revision|rev|r)\s?)(?P<revision>[\da-fA-F]+)(?P<post>\W+|$)'),
-                   "\g<prews><a href='http://sourceforge.net/p/roundup/code/ci/\g<revision>'>\g<revstr>\g<revision></a>\g<post>"),
+hg_url_base = r'http://sourceforge.net/p/roundup/code/ci/'
+
+substitutions = [ (re.compile(r'debian:\#(?P<id>\d+)'),
+                   r'<a href="http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=\g<id>">debian#\g<id></a>' ),
+                  (re.compile(r'\#(?P<ws>\s*)(?P<id>\d+)'),
+                   r"<a href='issue\g<id>'>#\g<ws>\g<id></a>" ),
+                  (re.compile(r'(?P<prews>^|\s+)issue(?P<ws>\s*)(?P<id>\d+)'),
+                   r"\g<prews><a href='issue\g<id>'>issue\g<ws>\g<id></a>" ),
+                  # matching the typical number:hash format of hg's own output
+                  # and then use use hash instead of the number
+                  (re.compile(r'(?P<prews>(^|\s+))(?P<revstr>(rev|hg))(?P<revnumber>\d+):(?P<refhash>[0-9a-fA-F]{12,40})(?P<post>\W+|$)'),
+                      r'\g<prews><a href="' + hg_url_base + '\g<refhash>">\g<revstr>\g<revnumber>:\g<refhash></a>\g<post>'),
+                  # matching hg revison number or hash
+                  (re.compile(r'(?P<prews>(^|\s+))(?P<revstr>(revision|rev|r)\s?)(?P<revision>([1-9][0-9]*)|[0-9a-fA-F]{4,40})(?P<post>\W+|$)'),
+                   r'\g<prews><a href="' + hg_url_base + '\g<revision>">\g<revstr>\g<revision></a>\g<post>'),
                   ]
 
 def local_replace(message):
@@ -19,16 +28,29 @@ def local_replace(message):
 def init(instance):
     instance.registerUtil('localReplace', local_replace)
 
+def quicktest(msgstr, should_replace = True):
+    if not should_replace:
+        print "(no)",
+    print "'%s' -> '%s'" % (msgstr, local_replace(msgstr))
 
 if "__main__" == __name__:
-    print " debian:#222", local_replace(" debian:#222")
-    print " #555", local_replace(" #555")
-    print " revision 222", local_replace(" revision 222")
-    print " r 222", local_replace(" r 222")
-    print " wordthatendswithr 222", local_replace(" wordthatendswithr 222") # should fail
-    print " references", local_replace(" references") # should fail
-    print " too many spaces r  222", local_replace(" too many spaces r  222") # should fail
-    print " r7140eb", local_replace(" r7140eb")
-    print " rev7140eb ", local_replace(" rev7140eb ")
-    print "rev7140eb", local_replace("rev7140eb")
-    print "rev7140eb,", local_replace("rev7140eb,")
+    print "Replacement examples. '(no)' should result in no replacement:"
+    quicktest(" debian:#222")
+    quicktest(" #555")
+    quicktest("issue333")
+    quicktest(" revision 222")
+    quicktest(" r 222")
+    quicktest(" wordthatendswithr 222", False)
+    quicktest(" references", False)
+    quicktest(" too many spaces r  222", False)
+    quicktest("re-evaluate", False)
+    quicktest("rex140eb", False)
+    quicktest("rev 012", False) # too short for a hg hash
+    quicktest("rev 0123")
+    quicktest("re140eb")
+    quicktest(" r7140eb")
+    quicktest(" rev7140eb ")
+    quicktest("rev7140eb")
+    quicktest("rev7140eb,")
+    quicktest("rev4891:ad3d628e73f2")
+    quicktest("hg4891:ad3d628e73f2")
--- website/issues/extensions/local_replace.py
+++ website/issues/extensions/local_replace.py
@@ -1,14 +1,20 @@
 import re
 
+hg_url_base = r'http://sourceforge.net/p/roundup/code/ci/'
+
 substitutions = [ (re.compile(r'debian:\#(?P<id>\d+)'),
                    r'<a href="http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=\g<id>">debian#\g<id></a>' ),
                   (re.compile(r'\#(?P<ws>\s*)(?P<id>\d+)'),
                    r"<a href='issue\g<id>'>#\g<ws>\g<id></a>" ),
                   (re.compile(r'(?P<prews>^|\s+)issue(?P<ws>\s*)(?P<id>\d+)'),
                    r"\g<prews><a href='issue\g<id>'>issue\g<ws>\g<id></a>" ),
+                  # matching the typical number:hash format of hg's own output
+                  # and then use use hash instead of the number
+                  (re.compile(r'(?P<prews>(^|\s+))(?P<revstr>(rev|hg))(?P<revnumber>\d+):(?P<refhash>[0-9a-fA-F]{12,40})(?P<post>\W+|$)'),
+                      r'\g<prews><a href="' + hg_url_base + '\g<refhash>">\g<revstr>\g<revnumber>:\g<refhash></a>\g<post>'),
                   # matching hg revison number or hash
-                  (re.compile(r'(?P<prews>^|\s+)(?P<revstr>(revision|rev|r)\s?)(?P<revision>([1-9][0-9]*)|[0-9a-fA-F]{4,40})(?P<post>\W+|$)'),
-                   r"\g<prews><a href='http://sourceforge.net/p/roundup/code/ci/\g<revision>'>\g<revstr>\g<revision></a>\g<post>"),
+                  (re.compile(r'(?P<prews>(^|\s+))(?P<revstr>(revision|rev|r)\s?)(?P<revision>([1-9][0-9]*)|[0-9a-fA-F]{4,40})(?P<post>\W+|$)'),
+                   r'\g<prews><a href="' + hg_url_base + '\g<revision>">\g<revstr>\g<revision></a>\g<post>'),
                   ]
 
 def local_replace(message):
@@ -46,3 +52,5 @@ if "__main__" == __name__:
     quicktest(" rev7140eb ")
     quicktest("rev7140eb")
     quicktest("rev7140eb,")
+    quicktest("rev4891:ad3d628e73f2")
+    quicktest("hg4891:ad3d628e73f2")
