455f0dbe67748f831d5b028cdce759f1969255a7
Bernhard Reiter <bernhard@intevation.de>
2016-01-05 22:16:54 +0100

    test_jinja2: stub improved, now with proper teardown.

--- test/test_jinja2.py
+++ test/test_jinja2.py
@@ -1,12 +1,15 @@
 #-*- encoding: utf8 -*-
 """ Testing the jinja2 templating engine of roundup-tracker.
 
-Copyright 2015 Bernhard E. Reiter <bernhard@intevation.de>
+Copyright: 2016 Intevation GmbH.
+Author: Bernhard E. Reiter <bernhard@intevation.de>
+
 This module is Free Software under the Roundup licensing of 1.5,
 see the COPYING.txt file coming with Roundup.
 
 Just a test file template for now.
 """
+import shutil # only, needed for tearDown. TODO: Remove when refactored.
 import unittest
 
 import db_test_base
@@ -18,6 +21,13 @@ class TestCase_Zero(unittest.TestCase):
         self.assertEqual(True, True)
 
 class TestCase(unittest.TestCase):
+    """Sets up and tears down an instance with database contents.
+
+    Setup and teardown modelled after the use of db_test_base
+    by several modules like test_xmlrpc and test_userauditor.
+
+    TODO: Should probably be moved to a base case in db_test_base.py.
+    """
 
     backend = None  # can be used to create tests per backend, see test_xmlrpc
 
@@ -26,8 +36,18 @@ class TestCase(unittest.TestCase):
         self.instance = db_test_base.setupTracker(self.dirname, self.backend)
         self.db = self.instance.open('admin')
 
+    def tearDown(self):
+        self.db.close()
+        try:
+            shutil.rmtree(self.dirname)
+        except OSError, error:
+            if error.errno not in (errno.ENOENT, errno.ESRCH): raise
+
     def test_zero(self):
+        """Do nothing just make sure that setup and teardown works."""
         pass
+
+
 
 def test_suite():
     suite = unittest.TestSuite()
@@ -37,14 +57,15 @@ def test_suite():
     # only using one database backend for now, not sure if doing all
     # backends will keep the test focussed enough to be useful for the used
     # computing time. Would be okay to change in the future.
-    l = 'anydbm'
-    dct = dict(backend = l)
-    subcls = type(TestCase)('TestCase_%s'%l, (TestCase,), dct)
-    suite.addTest(unittest.makeSuite(subcls))
+    # for l in list_backends():
+    for l in ['anydbm']:
+        dct = dict(backend = l)
+        subcls = type(TestCase)('TestCase_%s'%l, (TestCase,), dct)
+        suite.addTest(unittest.makeSuite(subcls))
 
     return suite
 
-# Other roundup testcases do have a def main thing in there,
+# Other roundup test source files have an if __name__ == '__main__': here
 # but I'm not sure if this is still helpful. So left out for now.
 
 # vim: ts=4 et sts=4 sw=4 ai :
