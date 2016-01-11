d61bfbc73a7297d307d9260179359c89ed66bba5
anatoly techtonik <techtonik@gmail.com>
2016-01-11 21:59:17 +0300

    Preparing 1.5.1  steps 7/16
    7. python setup.py build_doc
    
      * fix include of basic.css for rendering permalink anchors
      * make anchors less obtrusive

--- doc/conf.py
+++ doc/conf.py
@@ -126,7 +126,7 @@ pygments_style = 'sphinx'
 # The style sheet to use for HTML and HTML Help pages. A file of that name
 # must exist either in Sphinx' static/ path, or in one of the custom paths
 # given in html_static_path.
-html_style = 'default.css'
+html_style = 'style.css'
 
 #-- Sphinx 1.3
 # The theme to use for HTML and HTML Help pages.  See the documentation for
