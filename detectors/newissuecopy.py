53187e6610fbb129f6090a075a8309217c3928b9
Bernhard Reiter <bernhard@intevation.de>
2017-06-01 08:59:18 +0200

    OpenPGP: Extends newissuecopy.py to encrypt if configured.
    resolves issue2550945

--- detectors/newissuecopy.py
+++ detectors/newissuecopy.py
@@ -5,14 +5,19 @@ from roundup import roundupdb
 def newissuecopy(db, cl, nodeid, oldvalues):
     ''' Copy a message about new issues to a team address.
     '''
+    # get relevant crypto settings
+    encrypt = db.config.PGP_ENABLE and db.config.PGP_ENCRYPT
+
     # so use all the messages in the create
     change_note = cl.generateCreateNote(nodeid)
 
     # send a copy to the nosy list
     for msgid in cl.get(nodeid, 'messages'):
         try:
-            # note: last arg must be a list
-            cl.send_message(nodeid, msgid, change_note, ['team@team.host'])
+            # note: fourth arg must be a list
+            cl.send_message(nodeid, msgid, change_note,
+                            ['team@team.host'],
+                            crypt=encrypt)
         except roundupdb.MessageSendError, message:
             raise roundupdb.DetectorError, message
 
