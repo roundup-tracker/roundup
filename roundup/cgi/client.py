8ec68b0b3ee748a25465058743710db810e7c6cf
martin.v.loewis <martin.v.loewis>
2019-01-29 15:13:44 +0100

    Don't pass huge itemids into the backend.
    [[Ralf Schlatterbeck: Note that the original patch wasn't python3
    compatible and used long instead of int. Verified that python2.7 can
    deal with long numeric strings, e.g. 2**64 * 10:
    int("184467440737095516160")]]
    
    committer: Ralf Schlatterbeck <rsc@runtux.com>

--- roundup/cgi/client.py
+++ roundup/cgi/client.py
@@ -1351,6 +1351,10 @@ class Client:
                 klass = self.db.getclass(self.classname)
             except KeyError:
                 raise NotFound('%s/%s'%(self.classname, self.nodeid))
+            if int(self.nodeid) > 2**31:
+                # Postgres will complain with a ProgrammingError
+                # if we try to pass in numbers that are too large
+                raise NotFound ('%s/%s'%(self.classname, self.nodeid))
             if not klass.hasnode(self.nodeid):
                 raise NotFound('%s/%s'%(self.classname, self.nodeid))
             # with a designator, we default to item view
