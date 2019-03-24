5b8d7336d37850a7baf47dae6068343a93b464d6
Joseph Myers <jsm@polyomino.org.uk>
2019-03-24 21:49:17 +0000

    Adjust make_file override to use binary files only when needed.
    Previous version would produce errors for large text fields in form
    submissions with Python 3, so inherit from the default make_file
    implementation and only force binary files in the specific case
    (self.length >= 0) identified in <https://bugs.python.org/issue27777>.

--- roundup/cgi/client.py
+++ roundup/cgi/client.py
@@ -237,15 +237,13 @@ class BinaryFieldStorage(cgi.FieldStorage):
        needed for handling json and xml data blobs under python
        3. Under python 2, str and binary are interchangable, not so
        under 3.
-
-       Note that there may be places where this should support text mode.
-       (e.g. a large text file upload??). None are known, but this could be
-       a problem.
     '''
     def make_file(self, mode=None):
         ''' work around https://bugs.python.org/issue27777 '''
         import tempfile
-        return tempfile.TemporaryFile("wb+")
+        if self.length >= 0:
+            return tempfile.TemporaryFile("wb+")
+        return super().make_file()
 
 class Client:
     """Instantiate to handle one CGI request.
