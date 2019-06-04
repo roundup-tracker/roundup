a568e706c206033719c8e4f92e3c703618f8adab
John Rouillard <rouilj@ieee.org>
2019-06-04 18:42:05 -0400

    issue2551029: Jinja2 template install error.
    Issue where template's config.ini not getting updated.
    
    Change do_install in admin.py to load config_ini.ini from template
    before writing tracker's config.ini. This generates an updated
    config file for the user on install preserving values required tomake
    tracker work.
    
    Added config_ini.ini files to jinja2 and responsive templates to set
    required values (template_engine and static_files; static_files
    resp.).
    
    Documented new file in doc/tracker_templates.txt and added tests for
    new admin.py code.

--- test/test_admin.py
+++ test/test_admin.py
@@ -36,6 +36,38 @@ class AdminTest(object):
         self.assertTrue(ret == 0)
         self.assertTrue(os.path.isfile(self.dirname + "/config.ini"))
         self.assertTrue(os.path.isfile(self.dirname + "/schema.py"))
+
+    def testInitWithConfig_ini(self):
+        import sys
+        from roundup.configuration import CoreConfig
+        self.admin=AdminTool()
+        sys.argv=['main', '-i', '_test_admin', 'install', 'classic', self.backend]
+        # create a config_ini.ini file in classic template
+        templates=self.admin.listTemplates()
+        config_ini_content = "[mail]\n# comment\ndebug = SendMail.LOG\n"
+        config_ini_path = templates['classic']['path'] + '/config_ini.ini'
+        config_ini_file = open(config_ini_path, "w")
+        config_ini_file.write(config_ini_content)
+        config_ini_file.close()
+
+        try:
+            ret = self.admin.main()
+        finally:
+            try:
+                # ignore file not found
+                os.remove(config_ini_path)
+            except OSError as e:  # FileNotFound exception under py3
+                if e.errno == 2:
+                    pass
+                else:
+                    raise
+
+        print(ret)
+        self.assertTrue(ret == 0)
+        self.assertTrue(os.path.isfile(self.dirname + "/config.ini"))
+        self.assertTrue(os.path.isfile(self.dirname + "/schema.py"))
+        config=CoreConfig(self.dirname)
+        self.assertEqual(config['MAIL_DEBUG'], self.dirname + "/SendMail.LOG")
         
 
 
