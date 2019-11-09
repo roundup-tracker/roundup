361f1e2375d0ee8d4132282bfc784960977cd97c
John Rouillard <rouilj@ieee.org>
2019-11-09 00:41:53 -0500

    issue2550919 - Anti-bot signup using 4 second delay
    Oops another missing file 8-/.

---
+++ roundup/cgi/timestamp.py
@@ -0,0 +1,28 @@
+'''Set of functions of adding/checking timestamp to be used to limit
+   form submission for cgi actions.
+'''
+
+import time, struct, binascii, base64
+from roundup.cgi.exceptions import FormError
+from roundup.i18n import _
+from roundup.anypy.strings import b2s, s2b
+
+def pack_timestamp():
+    return b2s(base64.b64encode(struct.pack("i", int(time.time()))).strip())
+
+def unpack_timestamp(s):
+    try:
+        timestamp = struct.unpack("i",base64.b64decode(s2b(s)))[0]
+    except (struct.error, binascii.Error, TypeError) as e:
+        raise FormError(_("Form is corrupted."))
+    return timestamp
+
+class Timestamped:
+    def timecheck(self,field,delay):
+        try:
+            created = unpack_timestamp(self.form[field].value)
+        except KeyError:
+            raise FormError(_("Form is corrupted, missing: %s."%field))
+        if time.time() - created < delay:
+            raise FormError(_("Responding to form too quickly."))
+        return True
