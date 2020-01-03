73fde6c28f72a64f4f4d1b2a340b540003088ec0
John Rouillard <rouilj@ieee.org>
2020-01-02 21:04:21 -0500

    flake8 fixes replace bare except with except (ImportError, AttributeError)

--- roundup/anypy/http_.py
+++ roundup/anypy/http_.py
@@ -2,7 +2,7 @@ try:
     # Python 3+
     from http import client, server
     server.DEFAULT_ERROR_MESSAGE
-except:
+except (ImportError, AttributeError):
     # Python 2.5-2.7
     import httplib as client
     import BaseHTTPServer as server
