3e254468bb1f9c7ec65d2c6f03a24c38be8a4d75
John Rouillard <rouilj@ieee.org>
2021-04-26 23:39:30 -0400

    update copyright years.

--- doc/conf.py
+++ doc/conf.py
@@ -68,7 +68,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Roundup'
-copyright = u'2009-2020, Richard Jones, Roundup-Team'
+copyright = u'2009-2021, Richard Jones, Roundup-Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
