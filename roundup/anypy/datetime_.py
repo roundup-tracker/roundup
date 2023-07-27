566dd9f0cc2e82c0fafe78a673adbd3278f1b1ad
John Rouillard <rouilj@ieee.org>
2023-07-26 23:59:35 -0400

    fix: issue2551278 - datetime.datetime.utcnow deprecation.
    Original fix always failed the import and fell back to the exception case.
    This method should work and test both code paths in datetime_.py.

--- roundup/anypy/datetime_.py
+++ roundup/anypy/datetime_.py
@@ -1,12 +1,13 @@
 # https://issues.roundup-tracker.org/issue2551278
 # datetime.utcnow deprecated
 try:
-    from datetime import now, UTC
+    from datetime import datetime, UTC
 
     def utcnow():
-        return now(UTC)
+        return datetime.now(UTC)
+
 except ImportError:
-    import datetime
+    from datetime import datetime
 
     def utcnow():
-        return datetime.datetime.utcnow()
+        return datetime.utcnow()
