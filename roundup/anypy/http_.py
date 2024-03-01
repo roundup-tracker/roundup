171a80912ff397eb1ec736fa24c72ba6b3c3d1ce
John Rouillard <rouilj@ieee.org>
2024-03-01 15:08:42 -0500

    chore(lint): clean up ruff errors
    sort imports, remove unused statement

--- roundup/anypy/http_.py
+++ roundup/anypy/http_.py
@@ -1,8 +1,7 @@
 try:
     # Python 3+
     from http import client, server
-    server.DEFAULT_ERROR_MESSAGE
 except (ImportError, AttributeError):
     # Python 2.5-2.7
-    import httplib as client                     # noqa: F401
-    import BaseHTTPServer as server              # noqa: F401
+    import BaseHTTPServer as server  # noqa: F401
+    import httplib as client  # noqa: F401
