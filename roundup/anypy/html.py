f30e6f9ba1cb66dc77f5858945c0a2773fc8182f
John Rouillard <rouilj@ieee.org>
2024-03-02 04:14:18 -0500

    chore: ruff lint: replace str variable name with string
    str is a type in python.

--- roundup/anypy/html.py
+++ roundup/anypy/html.py
@@ -1,10 +1,10 @@
 try:
     from html import escape as html_escape_  # python 3
 
-    def html_escape(str, quote=False):
+    def html_escape(string, quote=False):
         # html_escape under python 3 sets quote to true by default
         # make it python 2 compatible
-        return html_escape_(str, quote=quote)
+        return html_escape_(string, quote=quote)
 except ImportError:
     # python 2 fallback
     from cgi import escape as html_escape  # noqa: F401
