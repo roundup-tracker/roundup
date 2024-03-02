aa57c9cbf81b9a896c7224518e3f11816dfa8102
John Rouillard <rouilj@ieee.org>
2024-03-02 04:10:40 -0500

    chore: sort imports w/ ruff.

--- roundup/anypy/cookie_.py
+++ roundup/anypy/cookie_.py
@@ -1,10 +1,10 @@
-
 try:
     # Python 3+
     from http import cookies as Cookie
-    from http.cookies import CookieError, BaseCookie, SimpleCookie
+    from http.cookies import BaseCookie, CookieError, SimpleCookie
     from http.cookies import _getdate as get_cookie_date
 except ImportError:
+    # ruff: noqa: F401, PLC2701
     # Python 2.5-2.7
-    from Cookie import CookieError, BaseCookie, SimpleCookie    # noqa: F401
-    from Cookie import _getdate as get_cookie_date              # noqa: F401
+    from Cookie import BaseCookie, CookieError, SimpleCookie
+    from Cookie import _getdate as get_cookie_date
--- roundup/anypy/datetime_.py
+++ roundup/anypy/datetime_.py
@@ -1,7 +1,7 @@
 # https://issues.roundup-tracker.org/issue2551278
 # datetime.utcnow deprecated
 try:
-    from datetime import datetime, UTC
+    from datetime import UTC, datetime
 
     def utcnow():
         return datetime.now(UTC)
