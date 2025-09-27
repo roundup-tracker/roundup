eedc6dec1dd7e8f76e6dfbd634519b6491e2dc41
John Rouillard <rouilj@ieee.org>
2025-09-27 17:26:32 -0400

    fix: make version_check enforce version is 3.7+

--- roundup/version_check.py
+++ roundup/version_check.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-# Roundup requires Python 2.7+ as mentioned in doc\installation.txt
+# Roundup requires Python 3.7+ as mentioned in doc\installation.txt
 from __future__ import print_function
 import sys
 
-VERSION_NEEDED = (2, 7)
+VERSION_NEEDED = (3, 7)
 
 if sys.version_info < VERSION_NEEDED:
     print("Content-Type: text/plain\n")
