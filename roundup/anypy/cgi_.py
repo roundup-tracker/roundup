40200f2716b9e84355abde308d108393d711e12a
John Rouillard <rouilj@ieee.org>
2025-12-15 17:31:37 -0500

    bug: supress deprecation warning when importing cgi module
    cgi was removed in 3.13, but it is vendored in Roundup.
    So we don't need to see the warning.

--- roundup/anypy/cgi_.py
+++ roundup/anypy/cgi_.py
@@ -1,7 +1,10 @@
 # ruff: noqa: F401  - unused imports
+import warnings
 try:
     # used for python2 and python 3 < 3.13
-    import cgi
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore', category=DeprecationWarning)
+        import cgi
     from cgi import FieldStorage, MiniFieldStorage
 except ImportError:
     # use for python3 >= 3.13
