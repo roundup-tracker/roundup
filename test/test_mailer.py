cc7d0ff4f7d37ac4d3e0073dbb09787c1babb994
John Rouillard <rouilj@ieee.org>
2026-07-13 08:17:37 -0400

    test: change header to use normalized encoding.
    per: https://github.com/python/cpython/issues/153633
    
    an encoded request for codec iso8859-1 should return a header with
    the normalized codec name: iso-8859-1.

--- test/test_mailer.py
+++ test/test_mailer.py
@@ -9,7 +9,7 @@ class EncodingTestCase(unittest.TestCase):
             a, c), o)
         a('ascii', 'ascii@test.com', 'iso8859-1', 'ascii <ascii@test.com>')
         a(u'café', 'ascii@test.com', 'iso8859-1',
-            '=?iso8859-1?q?caf=E9?= <ascii@test.com>')
+            '=?iso-8859-1?q?caf=E9?= <ascii@test.com>')
         a(u'café', 'ascii@test.com', 'utf-8',
             '=?utf-8?b?Y2Fmw6k=?= <ascii@test.com>')
         a('as"ii', 'ascii@test.com', 'iso8859-1', '"as\\"ii" <ascii@test.com>')
