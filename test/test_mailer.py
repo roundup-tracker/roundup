b23ba18191b62981a2645ac842363257537e365b
John Rouillard <rouilj@ieee.org>
2026-07-13 09:18:32 -0400

    test: change header to use normalized encoding. - py version issue
    Add conditional code to handle all Roundup supported python versions.
    
    ref: https://github.com/python/cpython/issues/153633

--- test/test_mailer.py
+++ test/test_mailer.py
@@ -1,4 +1,5 @@
 #-*- encoding: utf-8 -*-
+import sys
 import unittest
 
 from roundup import mailer
@@ -8,8 +9,13 @@ class EncodingTestCase(unittest.TestCase):
         a = lambda n, a, c, o: self.assertEqual(mailer.nice_sender_header(n,
             a, c), o)
         a('ascii', 'ascii@test.com', 'iso8859-1', 'ascii <ascii@test.com>')
-        a(u'café', 'ascii@test.com', 'iso8859-1',
-            '=?iso-8859-1?q?caf=E9?= <ascii@test.com>')
+        # FIXME 3.15 remove else case as it will not be needed anymore.
+        if sys.version_info >= (3, 15, 0):
+            a(u'café', 'ascii@test.com', 'iso8859-1',
+              '=?iso-8859-1?q?caf=E9?= <ascii@test.com>')
+        else:
+            a(u'café', 'ascii@test.com', 'iso8859-1',
+              '=?iso8859-1?q?caf=E9?= <ascii@test.com>')
         a(u'café', 'ascii@test.com', 'utf-8',
             '=?utf-8?b?Y2Fmw6k=?= <ascii@test.com>')
         a('as"ii', 'ascii@test.com', 'iso8859-1', '"as\\"ii" <ascii@test.com>')
