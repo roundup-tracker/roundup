cba085b2fd7661dca01594c1850017b08541a9b2
John Rouillard <rouilj@ieee.org>
2026-09-06 23:31:28 -0400

    bug: add reason to IndexError, reorder check for performance
    Reorder a check to match same method in rdbms_common.py. Slight perf
    improvment maybe.
    
    Also match IndexError to same code type in rdbms_common.py.

--- roundup/backends/back_anydbm.py
+++ roundup/backends/back_anydbm.py
@@ -1011,7 +1011,7 @@ class Class(hyperdb.Class):
 
         node = self.db.getnode(self.classname, nodeid)
         if self.db.RETIRED_FLAG in node:
-            raise IndexError
+            raise IndexError('Requested item is retired')
         num_re = re.compile(r'^\d+$')
 
         # if the journal value is to be different, store it in here
@@ -1122,11 +1122,19 @@ class Class(hyperdb.Class):
 
                 # handle additions
                 for id in value:
+                    if id in l:
+                        continue
+
+                    # We can safely check this condition after
+                    # checking that this is an addition to the
+                    # multilink since the condition was checked for
+                    # existing entries at the point they were added to
+                    # the multilink.  Since the hasnode call will
+                    # result in a new db query, it is more efficient to
+                    # avoid the check if possible.
                     if not self.db.getclass(link_class).hasnode(id):
                         raise IndexError('%s has no node %s' % (
                             link_class, id))
-                    if id in l:
-                        continue
                     # register the link with the newly linked node
                     if self.do_journal and self.properties[propname].do_journal:
                         self.db.addjournal(link_class, id, 'link',
